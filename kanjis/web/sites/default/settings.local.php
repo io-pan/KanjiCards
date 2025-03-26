649b1b4a0da5aac64b00abf6b5478555d1a499f1
ioPan <io-pan@users.noreply.github.com>
2025-03-26 20:23:59 +0100

    1st comimt

---
+++ kanjis/web/sites/default/settings.local.php
@@ -0,0 +1,123 @@
+<?php
+
+// phpcs:ignoreFile
+
+/**
+ * @file
+ * Local development override configuration feature.
+ *
+ * To activate this feature, copy and rename it such that its path plus
+ * filename is 'sites/default/settings.local.php'. Then, go to the bottom of
+ * 'sites/default/settings.php' and uncomment the commented lines that mention
+ * 'settings.local.php'.
+ *
+ * If you are using a site name in the path, such as 'sites/example.com', copy
+ * this file to 'sites/example.com/settings.local.php', and uncomment the lines
+ * at the bottom of 'sites/example.com/settings.php'.
+ */
+
+/**
+ * Assertions.
+ *
+ * The Drupal project primarily uses runtime assertions to enforce the
+ * expectations of the API by failing when incorrect calls are made by code
+ * under development.
+ *
+ * @see http://php.net/assert
+ * @see https://www.drupal.org/node/2492225
+ *
+ * It is strongly recommended that you set zend.assertions=1 in the PHP.ini file
+ * (It cannot be changed from .htaccess or runtime) on development machines and
+ * to 0 or -1 in production.
+ */
+
+/**
+ * Enable local development services.
+ */
+$settings['container_yamls'][] = DRUPAL_ROOT . '/sites/development.services.yml';
+
+/**
+ * Show all error messages, with backtrace information.
+ *
+ * In case the error level could not be fetched from the database, as for
+ * example the database connection failed, we rely only on this value.
+ */
+$config['system.logging']['error_level'] = 'verbose';
+
+/**
+ * Disable CSS and JS aggregation.
+ */
+$config['system.performance']['css']['preprocess'] = FALSE;
+$config['system.performance']['js']['preprocess'] = FALSE;
+
+/**
+ * Disable the render cache.
+ *
+ * Note: you should test with the render cache enabled, to ensure the correct
+ * cacheability metadata is present. However, in the early stages of
+ * development, you may want to disable it.
+ *
+ * This setting disables the render cache by using the Null cache back-end
+ * defined by the development.services.yml file above.
+ *
+ * Only use this setting once the site has been installed.
+ */
+# $settings['cache']['bins']['render'] = 'cache.backend.null';
+
+/**
+ * Disable caching for migrations.
+ *
+ * Uncomment the code below to only store migrations in memory and not in the
+ * database. This makes it easier to develop custom migrations.
+ */
+# $settings['cache']['bins']['discovery_migration'] = 'cache.backend.memory';
+
+/**
+ * Disable Internal Page Cache.
+ *
+ * Note: you should test with Internal Page Cache enabled, to ensure the correct
+ * cacheability metadata is present. However, in the early stages of
+ * development, you may want to disable it.
+ *
+ * This setting disables the page cache by using the Null cache back-end
+ * defined by the development.services.yml file above.
+ *
+ * Only use this setting once the site has been installed.
+ */
+# $settings['cache']['bins']['page'] = 'cache.backend.null';
+
+/**
+ * Disable Dynamic Page Cache.
+ *
+ * Note: you should test with Dynamic Page Cache enabled, to ensure the correct
+ * cacheability metadata is present (and hence the expected behavior). However,
+ * in the early stages of development, you may want to disable it.
+ */
+# $settings['cache']['bins']['dynamic_page_cache'] = 'cache.backend.null';
+
+/**
+ * Allow test modules and themes to be installed.
+ *
+ * Drupal ignores test modules and themes by default for performance reasons.
+ * During development it can be useful to install test extensions for debugging
+ * purposes.
+ */
+# $settings['extension_discovery_scan_tests'] = TRUE;
+
+
+$settings['rebuild_access'] = TRUE;
+$settings['skip_permissions_hardening'] = TRUE;
+// $settings['config_exclude_modules'] = ['devel', 'stage_file_proxy'];
+$settings['trusted_host_patterns'] = ['.*'];
+
+
+$databases['default']['default'] = array (
+  'database' => 'kanjis',
+  'username' => 'root',
+  'password' => 'root',
+  'prefix' => '',
+  'host' => 'localhost',
+  'port' => '3306',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'driver' => 'mysql',
+);
---
+++ kanjis/web/themes/custom/bootstrap_subtheme/js/global.js
@@ -0,0 +1,105 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function (Drupal, drupalSettings, once) {
+
+  Drupal.behaviors.fixLayout = {
+    attach(context) {
+      // Set printer margins to all recto.
+      const container = once('fixLayout', '.view-display-id-page_recto', context);
+      container.forEach(setMainMargin);
+
+      const containerS = once('fixLayout', '.view-display-id-verso_att', context);
+      containerS.forEach(setPageBreak);
+
+      const attachements = once('fixLayout2', '.view-display-id-verso_att', context);
+      attachements.forEach(setVersoPos);
+    }
+  };
+
+
+  function setMainMargin(domitem, index) {
+    console.log('setMainMargin',drupalSettings['printSettings']);
+    // Set padding
+    domitem.style.paddingTop    = drupalSettings['printSettings']['marginT']+'mm';
+    domitem.style.paddingBottom = drupalSettings['printSettings']['marginB']+'mm';
+    domitem.style.paddingRight  = drupalSettings['printSettings']['marginW']+'mm';
+    domitem.style.paddingLeft   = drupalSettings['printSettings']['marginW']+'mm';
+    setPageBreak(domitem, index);
+  }
+
+
+  function setPageBreak(domitem, index) {
+    console.log('setPageBreak', drupalSettings['printSettings']);
+
+    // Add page-break after each last card on page.
+    const pH =  297
+              - drupalSettings['printSettings']['marginT']
+              - drupalSettings['printSettings']['marginB'],
+          pW =  210
+              - 2*drupalSettings['printSettings']['marginW'],
+          cH =  60, // mm
+          cW =  35; // mm
+
+    cardsPerCol = Math.floor(pH / cH);
+    cardsPerRow = Math.floor(pW / cW);
+    cardsPerPage = cardsPerCol * cardsPerRow;
+        console.log('cardsPerCol',cardsPerCol);
+        console.log('cardsPerRow',cardsPerRow);
+        console.log('cardsPerPage',cardsPerPage);
+
+    itemlist = domitem.querySelectorAll(':scope > .view-content > .item-list > ul > li');
+    console.log('itemlist:', itemlist);
+    for (var i = cardsPerPage; i < itemlist.length; i+=cardsPerPage) {
+      let div = document.createElement("div");
+      div.classList.add("breakpage");
+      itemlist[i-1].after(div);
+
+      // give it a margin top to reach next page + let place for verso
+      // const spac = ( 297 - drupalSettings['printSettings']['marginT'] - (cH*cardsPerCol)
+      // Page break does the stuff ... give only space for verso   
+      itemlist[i].style.marginTop = 1*drupalSettings['printSettings']['marginT']
+                                    +297 +"mm";
+    }
+    // add last page break ... not needed.
+    // let div = document.createElement("div");
+    // div.classList.add("breakpage");
+    // itemlist[itemlist.length-1].after(div);
+  }
+
+  function setVersoPos(domitem, index) {
+    console.log('setVersoPos',drupalSettings['printSettings']);
+    const pH =  297
+              - drupalSettings['printSettings']['marginT']
+              - drupalSettings['printSettings']['marginB'],
+          pW = 210
+              - 2*drupalSettings['printSettings']['marginW'],
+          cH =  60, // mm
+          cW =  35; // mm
+
+    cardsPerCol = Math.floor(pH / cH);
+    cardsPerRow = Math.floor(pW / cW);
+
+    const top =  297 
+                + 1*drupalSettings['printSettings']['marginT']
+                +'mm',
+          width = cardsPerRow*cW+'mm';
+          // Verso est flippé avec un bête style direction: rtl;
+          // mais faut de toue façon  inverser la marge ...
+          // les marges gauche/droite de l'imprimante étant fixes
+          // on doit prendre la max pour les 2 cotés.
+          margeVerso =  210
+                      - 1*drupalSettings['printSettings']['marginW']
+                      - cardsPerRow*cW
+                      +'mm';
+
+    console.log('top, width, margeVerso:',top+ ", "+width+", "+margeVerso);
+    domitem.style.top = top;
+    domitem.style.width = width;
+    domitem.style.left = margeVerso;
+  }
+    
+}(Drupal, drupalSettings, once));
+
